00ac1bc0428eb9615b17f4b1a9b1e3cae62c54c7
Mathius <info@yogimathius.dev>
2024-05-15 22:10:24 -0700

    feat: complete ex02

---
+++ ex02/my_rust_journey_enum.rs
@@ -0,0 +1,19 @@
+enum Action {
+    Timer(u32),
+    Weather(String),
+    Music(String),
+}
+
+fn dispatch(action: Action) {
+    match action {
+        Action::Timer(int) => println!("AI -- Timer requested {}", int),
+        Action::Weather(message) => println!("AI -- Weather for this city {}", message),
+        Action::Music(song) => println!("AI -- Play following song requested {} requested", song),
+    }
+}
+
+fn main() {
+    dispatch(Action::Timer(32));
+    dispatch(Action::Music("Fly -- Ludovico Einaudi".to_string()));
+    dispatch(Action::Weather("Sedona - Arizona".to_string()));
+}
