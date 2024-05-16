5a54449dd2692beecfa805b58a58ab72ec61d2e9
Mathius <info@yogimathius.dev>
2024-05-15 22:10:43 -0700

    feat: complete ex03

---
+++ ex03/my_rust_journey_impl_method.rs
@@ -0,0 +1,36 @@
+struct Rectangle {
+    height: u32,
+    width: u32,
+}
+
+impl Rectangle {
+    fn new(height: u32, width: u32) -> Self {
+        Self { height, width }
+    }
+
+    fn area(&self) -> u32 {
+        return self.height * self.width;
+    }
+
+    fn can_hold(&self, rect2: &Rectangle) -> bool {
+        return rect2.height < self.height && rect2.width < self.width;
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle::new(30, 50);
+    let rect2 = Rectangle::new(10, 40);
+    let rect3 = Rectangle::new(60, 45);
+
+    println!("Rectangle 1 {} - {}", rect1.height, rect1.width);
+    println!("Rectangle 1 area {}", rect1.area());
+
+    println!(
+        "Can Rectangle #1 hold Rectangle #2? {}",
+        rect1.can_hold(&rect2)
+    );
+    println!(
+        "Can Rectangle #1 hold Rectangle #3? {}",
+        rect1.can_hold(&rect3)
+    );
+}
