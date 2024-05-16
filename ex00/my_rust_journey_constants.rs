2fcad1d242d078f7491c0de0e231668070ac3212
Mathius <info@yogimathius.dev>
2024-05-15 22:10:01 -0700

    feat: complete ex00

---
+++ ex00/my_rust_journey_constants.rs
@@ -0,0 +1,11 @@
+const SECONDS_IN_MINUTE: u32 = 60;
+const MINUTE_IN_HOUR: u32 = 60;
+const HOURS_IN_DAY: u32 = 24;
+const DAYS_IN_YEAR: u32 = 365;
+
+fn main() {
+    println!("Seconds in 1 minute {}.", SECONDS_IN_MINUTE);
+    println!("Minutes in 1 hour {}.", MINUTE_IN_HOUR);
+    println!("Hours in 1 day {}.", HOURS_IN_DAY);
+    println!("Days in 1 year {}.", DAYS_IN_YEAR);
+}
